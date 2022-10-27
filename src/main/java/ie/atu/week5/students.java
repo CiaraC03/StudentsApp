c46a1bcbe6d311fa6d7d901a936e0e6be59b957f
ciara <g00402357@atu.ie>
2022-10-27 14:36:05 +0100

    Prompt user for info

--- src/main/java/ie/atu/week5/students.java
+++ src/main/java/ie/atu/week5/students.java
@@ -1,4 +1,19 @@
 package ie.atu.week5;
 
+import java.util.Scanner;
 public class students {
+    public static void main(String[] args) {
+        System.out.println("Please enter first student name : ");
+        Scanner input = new Scanner(System.in);
+        String name = input.nextLine();
+        System.out.println("You entered : " + name);
+
+        System.out.println("Please enter your first email : ");
+        String email = input.nextLine();
+        System.out.println("You entered : " + email);
+
+        System.out.println("Please enter your first course : ");
+        String course = input.nextLine();
+        System.out.println("You entered : " + course);
+    }
 }
